61bdc16e8fb6429cccd16e416c22467e7b1bf2d2
Duckfeld <ivan.desc@gmail.com>
2020-04-25 16:19:59 -0700

    help.js main build

--- commands/help.js
+++ commands/help.js
@@ -1,18 +1,26 @@
-const Discord = require('discord.js');
+const { prefix } = require('../config.json');
 module.exports = {
 name: 'help',
 description: 'Shows help lol',
+usage: '[command name]',
 execute(message, args) {
-	const helpmenu = new Discord.MessageEmbed()
-		.setTitle('Help menu!')
-		.setAuthor('code by: Duck#8464 (Greego)')
-		.setDescription('The best duck bot ever.')
-		.addFields(
-		{ name: 'memcount', value: 'displays the member count of the server.' },
-		{ name: 'ping', value: 'pong!', inline: true },
-		{ name: 'duck', value: 'Shows a random duck!', inline: true },
-		{ name: 'gm/;gn', value: 'gm:(Good morning greeting), gn:(Good night greeting) \nYou must ping a user to do so.'},
-	);	
-		message.channel.send(helpmenu);
+	const data = [];
+	const { commands } = message.client;
+	if (!args.length) {
+	data.push('Here\'s a list of all my commands:');
+	data.push(commands.map(command => command.name).join(', '));
+	data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
+
+	return message.author.send(data, { split: true })
+	.then(() => {
+		if (message.channel.type === 'dm') return;
+		message.reply('I\'ve sent you a DM with all my commands!');
+	})
+	.catch(error => {
+		console.error(`Could not send help DM to ${message.author.tag}.\n`, error);
+		message.reply('it seems like I can\'t DM you! Do you have DMs disabled?');
+	});
+
+}
 	},
 };
