a21d26de430f0dfb0b02ec3841a0395cebb66094
Duckfeld <ivan.desc@gmail.com>
2020-04-25 10:20:54 -0700

    edit

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ client.on('message', async message => {
 	if (!client.commands.has(command)) return;
 
 try {
-	client.commands.get(command).execute(message, args, axios, gmduck, gnduck);
+	client.commands.get(command).execute(message, args, gmduck, gnduck, axios);
 } catch (error) {
 	console.error(error);
 	message.reply('there was an error trying to execute that command!');
